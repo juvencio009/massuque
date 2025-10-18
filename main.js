86328d3b31a92e74e0939911d8e1d248515a75b6
Juvêncio Filipe Penga <labmin.jf@gmail.com>
2025-10-18 22:20:41 +0200

    Melhorias na responsividade

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
 
-// Inicializa o EmailJS com sua Public Key
-(function() {
-  emailjs.init("A3BCR4UkSMijQwp65"); 
-})();
-
-// Manipula o envio do formulário
-document.getElementById("contact-form").addEventListener("submit",  function(e) {
-  e.preventDefault();
-
-  const statusMessage = document.getElementById("status-message");
-  statusMessage.textContent = "Enviando...";
-
-  // Envia o formulário usando EmailJS
-  emailjs.send("service_dbp5tle", "template_4u0ska6", {
-    from_name: document.getElementById("name").value,
-    from_email: document.getElementById("email").value,
-    message: document.getElementById("message").value
-  })
-  .then(() => {
-    statusMessage.textContent = "Mensagem enviada com sucesso! ✅";
-    statusMessage.style.color = "#4caf50";
-  })
-  .catch((error) => {
-    console.error("Erro ao enviar:", error);
-    statusMessage.textContent = "Erro ao enviar mensagem ❌. Tente novamente.";
-    statusMessage.style.color = "#f44336";
-  });
-});
 
 
 //toggle - menu
